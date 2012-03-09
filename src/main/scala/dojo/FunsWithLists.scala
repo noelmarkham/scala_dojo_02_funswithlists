d2a640f75078b8f6175f5220057711217d2282f4
Noel <noel.markham@playfish.com>
2012-03-09 17:00:09 +0000

    All working except one - try at home

--- src/main/scala/dojo/FunsWithLists.scala
+++ src/main/scala/dojo/FunsWithLists.scala
@@ -4,20 +4,39 @@ import collection.immutable.List
 
 object FunsWithLists {
 
-  def labels(ls: List[Game]) = Nil
+  def labels(ls: List[Game]) = ls map (x => x.label)
 
-  def averageRatingsOf(l:String,  ls:List[Game]) = 0
+  def averageRatingsOf(l:String,  ls:List[Game]) = {
+    val filteredList: List[Game] = ls filter (x => x.label == l)
+    (filteredList.foldLeft(0)((m: Int, n: Game) => m + n.rating)) / filteredList.size
+  }
 
-  def totalRatingsOf(ls: List[Game]):Int = 0
+  def totalRatingsOf(ls: List[Game]): Int = ls match {
+    case Nil => 0
+    case x :: xs => x.rating + totalRatingsOf(xs)
+  }
 
-  def totalRatingsOfLabel(label: String, list: List[Game]):Int = 0
+  def totalRatingsOfLabel(label: String, list: List[Game]):Int = {
+    var total = 0;
+    for(game <- list) {
+      if(game.label == label) {
+        total = total + game.rating
+      }
+    }
 
-  def increaseRatingBy(inc: Int, ls: List[Game]) = Nil
+    total
+  }
 
-  def decreaseRatingBy(i: Int, s: String, list: List[Game]) = Nil
+  def increaseRatingBy(inc: Int, ls: List[Game]) = ls map (x => Game(x.label, x.rating + inc))
 
-  def createFunctionToFindGamesByLabel(label: String):(List[Game]) => List[Game] = identity(_)
+  def decreaseRatingBy(i: Int, s: String, list: List[Game]): List[Game] = list match {
+    case Nil => Nil
+    case x :: xs if (x.label == s) => Game(s, x.rating - i) :: decreaseRatingBy(i, s, xs)
+    case x :: xs => x :: decreaseRatingBy(i, s, xs)
+  }
 
-  def zipWithKey = (f: (Game) => String, ls: List[Game]) => Nil
+  def createFunctionToFindGamesByLabel(label: String):(List[Game]) => List[Game] = _ filter(x => x.label == label)
+
+  def zipWithKey = (f: (Game) => String, ls: List[Game]) => labels(ls) map (x => x.toUpperCase) zip ls
 
 }
